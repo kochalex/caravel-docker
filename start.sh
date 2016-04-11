55d683cf8b4d4555cccc87d2481ef5dbee2416b4
aong <yxwzaxns@gmail.com>
2016-04-11 18:36:44 +0800

    test 5

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 fabmanager create-admin --app caravel --username admin --firstname caravel --lastname Administrator --email admin@test.com --password admin
 caravel db upgrade
 caravel init
+caravel runserver
