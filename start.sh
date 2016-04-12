d52818ffd4771050dc9330cf7a0bc04196b6a7cf
aong <yxwzaxns@gmail.com>
2016-04-12 11:38:58 +0800

    test 6

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-fabmanager create-admin --app caravel --username admin --firstname caravel --lastname Administrator --email admin@test.com --password admin
+fabmanager create-admin --app caravel --username admin --firstname caravel --lastname Administrator --email admin@test.com --password 1234
+echo "create user"
 caravel db upgrade
+echo "db upgrade"
 caravel init
+echo "app init"
 caravel runserver
